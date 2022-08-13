e905cb596e2f4dd6afd1cd4c1f4903d4cbfa8246
Flexxbo <flexxbo@gmail.com>
2022-08-14 00:13:57 +0200

    -i SHA256, addBlock(), generateHash()

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-dwad
+
+const Block = require('./block')
+const Blockchain = require('./blockchain')
+
+let genesisBlock = new Block()
+let blockchain = new Blockchain (genesisBlock)
+
+console.log(blockchain)
